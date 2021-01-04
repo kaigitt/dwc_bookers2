2462f36239e2fded273bfba5e9776624903151a9
EC2 Default User <ec2-user@ip-172-31-46-193.ec2.internal>
2021-01-04 08:19:24 +0000

    Initialized

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 2020_12_10_234342) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "profile_images", force: :cascade do |t|
-    t.string "image_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
